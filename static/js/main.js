1f196c01ddc88d83ae289cec05558754386b3b1a
Coco Liu <liuh3@rose-hulman.edu>
2016-10-09 21:13:02 -0400

    refactored jqury

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,9 @@
-$(document).ready(function() {
-    const registryToken = "772c16ee-3f8f-4484-8929-5cb5143da656";
+var rh = rh || {};
+rh.fm = rh.fm || {};
+
+
+rh.fm.loginRose = function() {
+	const registryToken = "772c16ee-3f8f-4484-8929-5cb5143da656";
     
     $('#login').click(function() {
     	Rosefire.signIn(registryToken, function(error, rosefireUser) {
@@ -10,4 +14,19 @@ $(document).ready(function() {
     		window.location.replace("/login?token=" + rosefireUser.token);
     	});
     });
+};
+	
+rh.fm.enableButtons = function() {
+	$('.insert-item').click(function() {
+		var item_url = $('#image_url').val();
+		var item_name = $('#item_name').val();
+		var item_description = $('#item_description').val();
+		var item_price = $('#item_price').val();
+		console.log(item_url);
+	});
+};
+
+$(document).ready(function() {
+	rh.fm.loginRose();
+	rh.fm.enableButtons();
 });
